ef7afffb0cb3177373bf0f0e5954bc2a99a8567c
Rafael Santana <rafael.santana@metasix.com.br>
2018-05-15 16:33:34 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/dungeonsanddragons/api/DungeonsAndDragonsApplication.java
@@ -0,0 +1,12 @@
+package dungeonsanddragons.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsAndDragonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonsAndDragonsApplication.class, args);
+	}
+}
